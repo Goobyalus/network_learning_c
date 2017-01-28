10352b6b930b28b5f016923864d4d4f5eb2030be
Goobyalus <unlisted@unlisted.unlisted>
2017-01-28 02:42:06 -0500

    updated readline

--- simple_chat/utils.c
+++ simple_chat/utils.c
@@ -2,15 +2,38 @@
 
 enum readline_success {
 	SUCCESS,
-	TOO_LONG};
+	TOO_LONG,
+	ERROR};
 
 /*	readline: read a line of console input up to bufsize
  *	arguments:
- *		prompt: in; string to display when requesting input
- *		
+ *		prompt:  in; string to display when requesting input
+ *		buffer: out; where to copy read input
+ *		bufsize: in; size of buffer
+ *	return value:
+ *		return an enum realine_success with result of read
+ *	Notes:
+ * 		It is caller's responsibility to zero buffer before calling.
  */
 enum readline_success readline(
 	const char* prompt, char *buffer, int bufsize) {
 	
+	// Print prompt if it exists
+	if (prompt) {
+		printf("%s", prompt);
+		fflush(stdout);
+	}
 	
+	// Read up to bufsize bytes
+	if (NULL == fgets(buffer, bufsize, stdin)) {
+		return ERROR;
+	}
+	if ('\n' != buffer[bufsize-1]) {
+		// Line did not fit in buffer - throw rest of line out
+		while ('\n' != getchar());
+		return TOO_LONG
+	}
+	
+	return SUCCESS;
 }
+
