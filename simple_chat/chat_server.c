1f4bb32616db0a2a7061c8206a4464d033f0d153
Goobyalus <unlisted@unlisted.unlisted>
2017-01-28 01:54:50 -0500

    working on simple chat program

---
+++ simple_chat/chat_server.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#include <errno.h>
+
+int main() {
+	
+	
+	
+	do {
+		
+	} while(
+	
+	return 0;
+}
---
+++ simple_chat/utils.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+enum readline_success {
+	SUCCESS,
+	TOO_LONG};
+
+/*	readline: read a line of console input up to bufsize
+ *	arguments:
+ *		prompt: in; string to display when requesting input
+ *		
+ */
+enum readline_success readline(
+	const char* prompt, char *buffer, int bufsize) {
+	
+	
+}
